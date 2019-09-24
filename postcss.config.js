3fa28b3ffa20ee83208fb1d3e487e4dd56b9bf15
SkyBlue <yakun1593@163.com>
2019-09-24 10:31:13 +0800

    修复错误

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+const autoprefixer = require('autoprefixer');
+const cssNextPreset = require('postcss-preset-env');
+
+module.exports = {
+    plugins: [
+        autoprefixer({
+            remove: false
+        }),
+        cssNextPreset({
+            stage: 3
+        }),
+    ]
+};
